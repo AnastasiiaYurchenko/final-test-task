9ec2929f6160b1bf8eec1c193e29b570b5d4b33e
AnastasiiaYurchenko <113210617+AnastasiiaYurchenko@users.noreply.github.com>
2023-09-06 08:20:15 +0000

    Deploying to gh-pages from @ AnastasiiaYurchenko/final-test-task@160212f4c22d13538bda90d084427a5445929960 🚀

--- static/js/720.708cb664.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[720],{720:function(e,t,a){a.r(t);a(791);var r=a(184);t.default=function(){return(0,r.jsx)("div",{children:"Catalog"})}}}]);
-//# sourceMappingURL=720.708cb664.chunk.js.map
